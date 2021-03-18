a30a337be88b1b5062cba0f17d15781602831aac
Dakaro <Dakaro_LS@o2.pl>
2021-03-19 00:45:09 +0100

    List of players without functions

--- wf_app/views.py
+++ wf_app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Player
 
-
-def index(request):
-    return HttpResponse("Helo world")
+def player_list(request):
+    players = Player.objects.all()
+    return render(request, "wf_app/wf_voting.html", {'players': players} )
